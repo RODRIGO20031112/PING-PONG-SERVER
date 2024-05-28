1cf0f5c96f09bc566edafacc7302a28c8e608d89
RODRIGO20031112 <rodrigo.bianco.carvalho@gmail.com>
2024-05-27 22:21:50 -0300

    INSERT - PING-PONG-SERVER - 27/05/2024

---
+++ index.js
@@ -0,0 +1,39 @@
+import bodyParser from "body-parser";
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const makeRequest = () => {
+  setTimeout(() => {
+    axios
+      .get(process.env.URL_STANDUP_SERV)
+      .then((response) => {
+        console.log("The adjacent answer is:", response.data);
+      })
+      .catch((error) => {
+        console.error("[ERROR] Resuming connection:", error.code);
+
+        setTimeout(() => {
+          makeRequest();
+        }, 1000);
+      });
+  }, 1000);
+};
+
+app.get("/standup/webhook/ping", (req, res) => {
+  makeRequest();
+  res.send("TradeGPT says pong");
+});
+
+setInterval(makeRequest, 10000);
+
+const PORT = process.env.PORT || 5555;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
